9c6e165385f94c160b414705dfab82c6cfc2738d
radoslav-zdravkovic-bc <zdravkovicradoslav@gmail.com>
2023-05-14 13:30:11 +0200

    remove unnecessary code and folders

--- views/meta-boxes/qm-title.php
+++ views/meta-boxes/qm-title.php
@@ -1,7 +1,6 @@
 <?php
 global $post;
 $qmTitleMeta = get_post_meta( $post->ID, 'qm-title', true );
-var_dump(get_post_meta( $post->ID));
 ?>
 
 <input type="text" name="qm-title" value="<?php echo esc_attr( $qmTitleMeta ) ?>">
